ca5940606f9447b1fa6a4d6c0036c029bc276c69
Alexey V.Ryapisov <tealikes@yandex.ru>
2019-10-01 02:33:43 +0300

    Implementing the Registration Controller and Final Authentication Features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
               .find_by(email: params['user']['email'])
@@ -9,9 +11,27 @@ class SessionsController < ApplicationController
         status: :created,
         logged_in: true,
         user: user
-      }else{
-        render json: { status: 401 }
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+  
+  def logged_in
+    if @current_user
+      render json:{
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
       }
     end
-  end 
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
 end
