9ea8b41313e14927ce8f08047aedcc25dfd09953
Alexey V.Ryapisov <tealikes@yandex.ru>
2019-10-01 16:22:45 +0300

    Configuring the Rails API Session Store to Work in All Environments

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -4,7 +4,7 @@ module CurrentUserConcern
   included do
     before_action :set_current_user
   end
-
+# находит пользователя по id сессии
   def set_current_user
     if session[:user_id]
       @current_user = User.find(session[:user_id])
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
-Rails.application.config.session_store :cookie_store, 
-  key: '_authentication_app',
-  domain: 'jdh-authentication-app-react.herokuapp.com'
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app',
+    domain: 'jdh-authentication-app-react.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
