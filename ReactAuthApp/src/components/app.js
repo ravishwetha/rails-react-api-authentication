d0e400aa0c6597b7ed940e8f8a9d79f7b04ab322
Alexey V.Ryapisov <tealikes@yandex.ru>
2019-10-02 15:25:23 +0300

    Check added, user logged in

--- ReactAuthApp/src/components/app.js
+++ ReactAuthApp/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import axios from 'axios';
 import Home from './Home';
 import Dashboard from './Dashboard';
 
@@ -7,6 +8,29 @@ export default class App extends Component {
   state = {
     loggedInStatus: "NOT_LOGGED_IN",
     user: {}
+  }
+
+  checkLoginStatus = () =>{
+    axios
+     .get("http://localhost:3000/logged_in", { withCredentials: true })
+     .then(response => {
+       if(response.data.logged_in && this.state.loggedInStatus == "NOT_LOGGED_IN"){
+         this.setState({
+           loggedInStatus: "LOGGED_IN",
+           user: response.data.user
+         })
+       }else if(!response.data.logged_in && this.state.loggedInStatus == "LOGGED_IN"){
+         this.setState({
+           loggedInStatus: "NOT_LOGGED_IN",
+           user: {}
+         })
+       }
+     })
+     .catch(error => {console.log("check loggin error", error)})
+  }
+
+  componentDidMount(){
+    this.checkLoginStatus();
   }
 
   handleLogin = (data) =>{
